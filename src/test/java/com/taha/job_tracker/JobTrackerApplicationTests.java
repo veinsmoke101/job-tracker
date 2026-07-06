9dc10f239e0d2259b830d66b84b37ff7104a9bbd
tahalechgar <tahamr08@gmail.com>
2026-07-06 16:51:46 +0100

    created project skeleton with required dependencies

---
+++ src/test/java/com/taha/job_tracker/JobTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taha.job_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
